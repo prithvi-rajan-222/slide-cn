b04a27418d4163076622879d10dad13fcac9758f
Prithvi Rajan <prithvirajan222@gmail.com>
2026-01-07 21:47:39 +0530

    temp

---
+++ components/fourth-slide.tsx
@@ -0,0 +1,12 @@
+import { HeaderWithContent } from "@/registry/slide-cn/header-with-content/header-with-content";
+
+export function FourthSlide() {
+	return (
+		<HeaderWithContent>
+			<HeaderWithContent.Header>How is Slide-CN better than existing tools?</HeaderWithContent.Header>
+			<HeaderWithContent.Content className="flex flex-row gap-4">
+				{/* add a card style content here */}
+			</HeaderWithContent.Content>
+		</HeaderWithContent>
+	)
+}
