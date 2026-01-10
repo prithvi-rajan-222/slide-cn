29a1bbc13f849cfa5ecf83e54f35511a2597a6c4
Prithvi Rajan <prithvirajan222@gmail.com>
2026-01-10 22:25:32 +0530

    added cat

--- components/second-slide.tsx
+++ components/second-slide.tsx
@@ -1,4 +1,5 @@
 import { HeaderWithContent } from "@/registry/slide-cn/header-with-content/header-with-content";
+import { SlideImage } from "./ui/slide-image";
 
 export function SecondSlide() {
 	return (
@@ -6,7 +7,7 @@ export function SecondSlide() {
 			<HeaderWithContent.Header>
 				What is Slide-CN?
 			</HeaderWithContent.Header>
-			<HeaderWithContent.Content>
+			<HeaderWithContent.Content className="flex flex-col gap-4">
 				<ul className="list-disc list-inside flex flex-col gap-2">
 					<li>Slide-CN is a react component library with which you can create beautiful presentations using code</li>
 					<li>Slide-CN handles slide transitions, mobile layouts and more</li>
@@ -15,6 +16,14 @@ export function SecondSlide() {
 					<li>Use the basic templates provided by Slide-CN and extend them to create your own branding, your own themes, so that your presentations can be consistent</li>
 					<li>Slide-CN works with ShadCN components, so you can be as creative as you want with your slides</li>
 				</ul>
+				<SlideImage
+					src="/spinning-cat.gif"
+					alt="oiia cat"
+					aspectRatio={1}
+					caption="Spinning cat"
+					objectFit="contain"
+					className="self-center w-[25%] h-full"
+				/>
 			</HeaderWithContent.Content>
 		</HeaderWithContent>
 	)
