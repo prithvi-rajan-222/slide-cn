33d681838d1a352a14914922320b2de73c6dfbfa
Prithvi Rajan <prithvirajan222@gmail.com>
2026-01-16 19:01:07 -0800

    vertical split is working

--- components/second-slide.tsx
+++ components/second-slide.tsx
@@ -2,6 +2,7 @@
 import { HeaderWithContent } from "@/registry/slide-cn/header-with-content/header-with-content";
 import { SlideImage } from "@/registry/slide-cn/slide-image/slide-image";
 import LiquidEther from "./LiquidEther.jsx"
+import { TwoColumn } from "@/registry/slide-cn/two-column/two-column";
 
 export function SecondSlide() {
 	return (
@@ -30,14 +31,34 @@ export function SecondSlide() {
 					What is Slide-CN?
 				</HeaderWithContent.Header>
 				<HeaderWithContent.Content className="flex flex-col gap-4">
-					<ul className="list-disc list-inside flex flex-col gap-2">
-						<li>Slide-CN is a react component library with which you can create beautiful presentations using code</li>
-						<li>Slide-CN handles slide transitions, mobile layouts and more</li>
-						<li>It works out of the box with smart defaults, so you can create presentations that look good with minimal effort</li>
-						<li>You own all of the code used to create presentations. It is not hidden behind a library. Slide-CN is open-source and open-code</li>
-						<li>Use the basic templates provided by Slide-CN and extend them to create your own branding, your own themes, so that your presentations can be consistent</li>
-						<li>Slide-CN works with ShadCN components, so you can be as creative as you want with your slides</li>
-					</ul>
+					<TwoColumn ratio={0.35}>
+						<TwoColumn.Left>
+
+
+							<ul className="list-disc list-inside flex flex-col gap-2">
+								<li>Slide-CN is a react component library with which you can create beautiful presentations using code</li>
+								<li>Slide-CN handles slide transitions, mobile layouts and more</li>
+								<li>It works out of the box with smart defaults, so you can create presentations that look good with minimal effort</li>
+								<li>You own all of the code used to create presentations. It is not hidden behind a library. Slide-CN is open-source and open-code</li>
+								<li>Use the basic templates provided by Slide-CN and extend them to create your own branding, your own themes, so that your presentations can be consistent</li>
+								<li>Slide-CN works with ShadCN components, so you can be as creative as you want with your slides</li>
+							</ul>
+						</TwoColumn.Left>
+						<TwoColumn.Right>
+
+							<SlideImage className="my-auto">
+								<SlideImage.Image
+									src="/spinning-cat.gif"
+									alt="oiia cat"
+									aspectRatio={1}
+									objectFit="contain"
+									className="w-[25%]"
+								/>
+								<SlideImage.Caption>Spinning cat</SlideImage.Caption>
+							</SlideImage>
+						</TwoColumn.Right>
+					</TwoColumn>
+
 					<SlideImage className="my-auto">
 						<SlideImage.Image
 							src="/spinning-cat.gif"
@@ -49,7 +70,7 @@ export function SecondSlide() {
 						<SlideImage.Caption>Spinning cat</SlideImage.Caption>
 					</SlideImage>
 				</HeaderWithContent.Content>
-			</HeaderWithContent>
+			</HeaderWithContent >
 		</>
 	)
 }
--- registry/slide-cn/two-column/two-column.tsx
+++ registry/slide-cn/two-column/two-column.tsx
@@ -1,8 +1,20 @@
 import { cn } from "@/lib/utils";
 
-export function TwoColumn({ children }: React.ComponentProps<"div">) {
+interface TwoColumnProps extends React.ComponentProps<"div"> {
+	ratio?: number;
+}
+
+export function TwoColumn({ children, ratio = 0.5, className, style, ...props }: TwoColumnProps) {
+	const clampedRatio = Math.max(0.2, Math.min(0.8, ratio));
 	return (
-		<div className="grid h-full w-full grid-cols-2 gap-8 items-center">
+		<div
+			className={cn("grid h-full w-full grid-cols-2 gap-8 items-center", className)}
+			style={{
+				gridTemplateColumns: `${clampedRatio}fr ${1 - clampedRatio}fr`,
+				...style,
+			}}
+			{...props}
+		>
 			{children}
 		</div>
 	);
