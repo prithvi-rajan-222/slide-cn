2c820b357b05380797b4057118d7ed861e4a05d8
Prithvi Rajan <prithvirajan222@gmail.com>
2026-01-19 22:33:10 -0800

    added more docs

--- .source/server.ts
+++ .source/server.ts
@@ -1,9 +1,12 @@
 // @ts-nocheck
-import * as __fd_glob_7 from "../content/docs/layouts/vertical-split.mdx?collection=docs"
-import * as __fd_glob_6 from "../content/docs/layouts/title-slide.mdx?collection=docs"
-import * as __fd_glob_5 from "../content/docs/layouts/index.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/layouts/horizontal-split.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/layouts/header-with-content.mdx?collection=docs"
+import * as __fd_glob_10 from "../content/docs/layouts/vertical-split.mdx?collection=docs"
+import * as __fd_glob_9 from "../content/docs/layouts/title-slide.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/layouts/index.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/layouts/horizontal-split.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/layouts/header-with-content.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/components/slide.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/components/slide-image.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/components/reveal.mdx?collection=docs"
 import * as __fd_glob_2 from "../content/docs/components/index.mdx?collection=docs"
 import * as __fd_glob_1 from "../content/docs/components/deck.mdx?collection=docs"
 import * as __fd_glob_0 from "../content/docs/index.mdx?collection=docs"
@@ -15,4 +18,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {}, {"index.mdx": __fd_glob_0, "components/deck.mdx": __fd_glob_1, "components/index.mdx": __fd_glob_2, "layouts/header-with-content.mdx": __fd_glob_3, "layouts/horizontal-split.mdx": __fd_glob_4, "layouts/index.mdx": __fd_glob_5, "layouts/title-slide.mdx": __fd_glob_6, "layouts/vertical-split.mdx": __fd_glob_7, });
+export const docs = await create.docs("docs", "content/docs", {}, {"index.mdx": __fd_glob_0, "components/deck.mdx": __fd_glob_1, "components/index.mdx": __fd_glob_2, "components/reveal.mdx": __fd_glob_3, "components/slide-image.mdx": __fd_glob_4, "components/slide.mdx": __fd_glob_5, "layouts/header-with-content.mdx": __fd_glob_6, "layouts/horizontal-split.mdx": __fd_glob_7, "layouts/index.mdx": __fd_glob_8, "layouts/title-slide.mdx": __fd_glob_9, "layouts/vertical-split.mdx": __fd_glob_10, });
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,6 @@
 import defaultMdxComponents from "fumadocs-ui/mdx"
 import type { MDXComponents } from "mdx/types"
-
+import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
 import {
 	Tabs,
 	TabsList,
@@ -10,19 +10,24 @@ import {
 import {
 	TypeTable
 } from "fumadocs-ui/components/type-table";
-
+import { InstallCommand } from "./components/docs/InstallCommand"
 export function getMDXComponents(
 	components?: MDXComponents
 ): MDXComponents {
 	return {
 		...defaultMdxComponents,
-
+		pre: ({ ref: _ref, ...props }) => (
+			<CodeBlock>
+				<Pre>{props.children}</Pre>
+			</CodeBlock>
+		),
 		// Tabs
 		Tabs,
 		TabsList,
 		TabsTrigger,
 		TabsContent,
 		TypeTable,
+		InstallCommand,
 
 		...components,
 	}
