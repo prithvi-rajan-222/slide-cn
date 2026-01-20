0861625db1cec2e642c40df70a414225fb5f1baf
Prithvi Rajan <prithvirajan222@gmail.com>
2026-01-19 16:51:24 -0800

    continuing with docs work

--- .source/server.ts
+++ .source/server.ts
@@ -1,9 +1,11 @@
 // @ts-nocheck
-import * as __fd_glob_5 from "../content/docs/layouts/vertical-split.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/layouts/title-slide.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/layouts/index.mdx?collection=docs"
-import * as __fd_glob_2 from "../content/docs/layouts/horizontal-split.mdx?collection=docs"
-import * as __fd_glob_1 from "../content/docs/layouts/header-with-content.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/layouts/vertical-split.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/layouts/title-slide.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/layouts/index.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/layouts/horizontal-split.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/layouts/header-with-content.mdx?collection=docs"
+import * as __fd_glob_2 from "../content/docs/components/index.mdx?collection=docs"
+import * as __fd_glob_1 from "../content/docs/components/deck.mdx?collection=docs"
 import * as __fd_glob_0 from "../content/docs/index.mdx?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../source.config';
@@ -13,4 +15,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {}, {"index.mdx": __fd_glob_0, "layouts/header-with-content.mdx": __fd_glob_1, "layouts/horizontal-split.mdx": __fd_glob_2, "layouts/index.mdx": __fd_glob_3, "layouts/title-slide.mdx": __fd_glob_4, "layouts/vertical-split.mdx": __fd_glob_5, });
+export const docs = await create.docs("docs", "content/docs", {}, {"index.mdx": __fd_glob_0, "components/deck.mdx": __fd_glob_1, "components/index.mdx": __fd_glob_2, "layouts/header-with-content.mdx": __fd_glob_3, "layouts/horizontal-split.mdx": __fd_glob_4, "layouts/index.mdx": __fd_glob_5, "layouts/title-slide.mdx": __fd_glob_6, "layouts/vertical-split.mdx": __fd_glob_7, });
