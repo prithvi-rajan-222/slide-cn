c87ba9a3cbb9ced94e6939fe098d5eb308109854
Prithvi Rajan <prithvirajan222@gmail.com>
2026-01-22 17:00:28 -0800

    testing branch protection

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from '@vercel/analytics/next';
 
+
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
