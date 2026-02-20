5881919669e9cd7c20465af8de58b1e64fb521eb
Prithvi Rajan <prithvirajan222@gmail.com>
2026-02-19 16:30:55 -0800

    removed theme switcher

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -5,7 +5,12 @@ import { baseOptions } from '@/lib/layout.shared';
 export default function Layout({ children }: LayoutProps<'/docs'>) {
 	return (
 		<>
-			<DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+			<DocsLayout
+				tree={source.getPageTree()}
+				themeSwitch={{ enabled: false }}
+				{...baseOptions()}
+
+			>
 				{children}
 			</DocsLayout>
 		</>
